a94adb387d01872b9cb8e646766747a733aa6852
paulritzman <ritzman.paul@gmail.com>
2018-05-24 12:39:41 -0700

    started work on enabling users to post books to database

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ const cors = require('cors');
 const pg = require('pg');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.use(cors());
 
-// const conString = 'postgres://localhost:5432';
-const conString = 'postgres://postgres:password@localhost:5432/postgres';
-
-const client = new pg.Client(process.env.DATABASE_URL || conString);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
-
 
 //root request
 app.get('/', (req, res) => {
@@ -26,6 +22,7 @@ app.get('/', (req, res) => {
     .catch(result => res.sendStatus(404).send(result));
 });
 
+// Retrieve a single book based on ID
 app.get('/api/v1/books/:id', (req, res) => {
   let SQL = 'SELECT * FROM books WHERE book_id = $1;';
   let values = [req.params.id];
@@ -35,14 +32,22 @@ app.get('/api/v1/books/:id', (req, res) => {
     .catch(result => res.sendStatus(404).send(result));
 });
 
-// app.post('post-new-book', (req, res) => {
-//   console.log(req);
-// });
+app.post('/submitted', (req, res) => {
+  let {title, author, isbn, image_url, description} = req.body;
+  let values = [title, author, isbn, image_url, description];
+
+  let SQL = `INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);`;
+
+  client.query(SQL, values)
+    .then(result => res.send(result.rows))
+    .catch(result => res.sendStatus(404).send(result));
+
+});
 
 //catch-all request
 app.get('*', (req, res) => res.status(403).send('This route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
-// Mac: export DATABASE_URL=postgres://localhost:5432/books_app
+// Mac: export DATABASE_URL=postgres://localhost:5432/
 // Windows: export DATABASE_URL=postgres://postgres:password@localhost:5432/books_app
